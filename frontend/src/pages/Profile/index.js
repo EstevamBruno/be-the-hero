34508b58e0accffb7cb5b47878ec7c8c5c714ef6
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-12 13:11:16 -0300

    front-end do app

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -69,7 +69,7 @@ export default function Profile() {
             <strong>DESCRIÇÃO:</strong>
             <p>{incident.description}</p>
             <strong>VALOR:</strong>
-            <p>{incident.value}</p>
+            <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
             <button
               type="button"
               onClick={() => handleDeleteIncident(incident.id)}
